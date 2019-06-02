245d574326a14b3afde2c1679582d36df2077d08
Lukas Zierahn <lukas@kappa-mm.de>
2019-06-02 12:42:58 +0100

    Initial Commit

---
+++ PLG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PLG
+//
+//  Created by Lukas Zierahn on 02.06.19.
+//  Copyright © 2019 Lukas Zierahn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
